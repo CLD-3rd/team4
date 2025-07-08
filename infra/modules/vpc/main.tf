b3d58b5cde086558760f941112ff7a2b25f5599d
seoulcloud <integrall92@gmail.com>
2025-07-08 16:50:55 +0900

    feat : vpc/main.tf vpc 및 네트워크설정

---
+++ infra/modules/vpc/main.tf
@@ -0,0 +1,91 @@
+### VPC 설정 모듈 (요약:1VPC 1Private 1Public ) - 김재신
+resource "aws_vpc" "vpc1" { # vpc 생성
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public1" { # 퍼블릭 subnet
+  vpc_id            = aws_vpc.vpc1.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = var.az # 서울 a 영역
+  map_public_ip_on_launch = true # 퍼블릭
+  tags = {
+    Name = var.public_subnet_name
+  }
+}
+
+resource "aws_subnet" "private1" { #프라이빗 subnet
+  vpc_id            = aws_vpc.vpc1.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.az # 서울 a 영역
+  map_public_ip_on_launch = false # 프라이빗
+  tags = {
+    Name = var.private_subnet_name
+  }
+} 
+
+# 인터넷 게이트웨이
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc1.id
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+# NAT용 EIP
+resource "aws_eip" "nat_eip" {
+  vpc = true
+  tags = {
+    Name = "${var.vpc_name}-nat-eip"
+  }
+}
+
+# NAT 게이트웨이
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public1.id
+  tags = {
+    Name = "${var.vpc_name}-nat"
+  }
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# 퍼블릭 라우트 테이블
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc1.id
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# 프라이빗 라우트 테이블
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc1.id
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+
+resource "aws_route" "private_nat_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+} 
