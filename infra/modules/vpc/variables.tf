9a58424ab12fdb5dc7bb6a8778c99a4912b69d99
wonshee <shee2.won@gmail.com>
2025-07-10 10:14:58 +0900

    refactor: add VPC tag

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -1,5 +1,11 @@
 ### VPC main 의 Variable 변수설정 - 김재신
 
+# 250710 원서희: Project 관련 태그 변수 추가
+variable "project" {
+  description = "Project name for tagging or resource naming"
+  type        = string
+}
+
 variable "vpc_name" {
   description = "VPC 이름"
   type        = string
